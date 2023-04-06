feb4028376e2862bd27787eac15aeeb418e76d0a
Eduardo Donini <eduardodonini1311@gmail.com>
2023-04-06 18:15:31 -0300

    first

---
+++ src/app/note-card/note-card.component.ts
@@ -0,0 +1,34 @@
+import {
+  Component,
+  ViewChild,
+  ElementRef,
+  Renderer2,
+  AfterViewInit,
+  Input,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-note-card',
+  templateUrl: './note-card.component.html',
+  styleUrls: ['./note-card.component.scss'],
+})
+export class NoteCardComponent implements AfterViewInit {
+  @Input() title: string = '';
+  @Input() body: string = '';
+
+  @ViewChild('truncator', { static: true }) truncator!: ElementRef<HTMLElement>;
+  @ViewChild('bodyText', { static: true }) bodyText!: ElementRef<HTMLElement>;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    let style = window.getComputedStyle(this.bodyText.nativeElement, null);
+    let viewableHeight = parseInt(style.getPropertyValue('height'), 10);
+
+    if (this.bodyText.nativeElement.scrollHeight > viewableHeight) {
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
+    } else {
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none');
+    }
+  }
+}
