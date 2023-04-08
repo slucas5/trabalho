bbd9e394bce57439e87c4b9eb056eead95d8cabb
Lucas <lucassouzasilva23@hotmail.com>
2023-04-08 19:22:38 -0300

    Transições
    Transições para add e excluir nota (4º)

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,3 +1,4 @@
+import { animate, style, transition, trigger, query, stagger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Note } from 'src/app/shared/note.model';
 import { NotesService } from 'src/app/shared/notes.service';
@@ -6,6 +7,78 @@ import { NotesService } from 'src/app/shared/notes.service';
   selector: 'app-notes-list',
   templateUrl: './notes-list.component.html',
   styleUrls: ['./notes-list.component.scss'],
+  animations: [
+    trigger('itemAnim',[
+      //entry animation
+      transition('void => *', [
+        // Inital state
+        style({
+          height: 0,
+          opacity: 0,
+          transform: 'scale(0,85',
+          'margin-bottom': 0,
+
+          // wa have to 'expand' out the padding properties
+          paddingTop: 0,
+          paddingBottom: 0,
+          paddingLeft: 0,
+          paddingRight: 0,
+        }),
+        //we first was to animante spacing (which include margin and height)
+        animate('50ms', style({
+          height: '*',
+          'margin-bottom': '*',
+          paddingTop: '*',
+          paddingBottom: '*',
+          paddingLeft: '*',
+          paddingRight: '*',
+        })),
+        animate(68)
+      ]),
+      transition('* => void', [
+        //first scale up
+        animate(50, style({
+          transform: 'scale(1.05)'
+        })),
+        //then scale down ack to normal size while beginning to fade out
+        animate(50, style({
+          transform: 'scale(1)',
+          opacity: 0.75
+        })),
+        //scale down and fade out completely
+        animate('120ms ease-out', style({
+          transform: 'scale(0.68)',
+          opacity: 0,
+        })),
+        //then animate the spacing  (which include height, margin and padding)
+        animate('150ms ease-out', style({
+          opacity: 0,
+          height: 0,
+          paddingTop: 0,
+          paddingBottom: 0,
+          paddingRight: 0,
+          paddingLeft: 0,
+          'margin-bottom': 0,
+        }))
+      ])
+    ]),
+
+    trigger('listAnim', [
+      transition('* => *', [
+        query(':enter', [
+          style({
+            opacity: 0,
+            height: 0
+          }),
+          stagger(100, [
+            animate('0.2s ease')
+          ])
+        ], {
+          optional: true
+        })
+      ])
+    ])
+  ]
 })
 export class NotesListComponent implements OnInit{
  
