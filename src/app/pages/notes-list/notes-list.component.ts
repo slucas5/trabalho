54ffc6d51f4db448feb281c79cd175fb040fa122
Lucas <lucassouzasilva23@hotmail.com>
2023-04-08 17:38:59 -0300

    Editando nota
    Fiz até o min 51 do vídeo, com alguns ajustes na parte de editar, pois do jeito do video deu um erro.  Então criei uma rota fixa.

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Note } from 'src/app/shared/note.model';
+import { NotesService } from 'src/app/shared/notes.service';
 
 @Component({
   selector: 'app-notes-list',
   templateUrl: './notes-list.component.html',
   styleUrls: ['./notes-list.component.scss'],
 })
-export class NotesListComponent {
-  cardTitle: string = 'abc';
+export class NotesListComponent implements OnInit{
+ 
+  notes: Note[] = new Array<Note>();
+
+  constructor(private notesService: NotesService) { }
+
+  ngOnInit() {
+    //we want to retrieve all notes from NotesService
+    this.notes = this.notesService.getAll();
+  }
 }
